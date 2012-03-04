031d3387f924496af80a9c9ba450ab48deb90a3a
Jeff Meyerson <meff.jeyerson@gmail.com>
2012-03-04 17:28:40 -0600

    Minor changes, fiddling with unit tests

--- src/static_objects/DegradationQueue.java
+++ src/static_objects/DegradationQueue.java
@@ -9,25 +9,43 @@ import java.util.ArrayList;
  * At the beginning of each turn, the function at the front of the queue is degraded.
  */
 
-public class DegradationQueue {
+public final class DegradationQueue {
 
 	private static final ArrayList<Function> dq = new ArrayList<Function>();
 	
 	//Degrade and return the front function
-	public Function degradeFront(){
+	public static Function degradeFront(){
 		Function degraded = dq.get(dq.size() - 1);
 		dq.remove(dq.size() - 1);
 		return degraded;
 	}
 	
-	public void enqueue(Function f){
+	public static void degrade(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void enqueue(Function f){
 		dq.add(f);
 	}
 	
 	//future implementation should use IO with some higher level class that does the printing
-	public void print(){
+	public static void print(){
 		System.out.println(dq.toString());
 	}
+	
+	public String toString(){
+		return dq.toString();
+	}
+
+	public static int size() {
+		return dq.size();
+	}
+
+	public static boolean empty() {
+		return dq.size() == 0;
+	}
+
 	
 	
 	

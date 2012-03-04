2eba0c2f81f94e47e37e4137f3f2cd9f823a6382
Jeff Meyerson <meff.jeyerson@gmail.com>
2012-03-03 22:26:13 -0600

    added example construction/instance pair

---
+++ src/constructions/LactiferousAntennaeConstruction.java
@@ -0,0 +1,32 @@
+package constructions;
+
+
+/*
+ * Technical outline:
+ * 
+ * Lactiferous Antennae--ee
+ * 1/4
+ * When Lactiferous Antennae is instantiated, your opponent exposes a random function from his function bag.  
+ * It remains exposed until it leaves his function bag.  If that function leaves his function bag, your opponent
+ * exposes a different function from his function bag at random.
+ * Extraction: ggg
+ * 
+ * Creative outline:
+ * 
+ * A comparatively small pair or trio of antennae that are capable of biologically detecting
+ * a small amount of indiscriminate information.  They have weak, but not minimal offensive capability.
+ * They lactate significantly, making them valued primarily as a resource piece.
+ */
+
+public class LactiferousAntennaeConstruction extends Construction {
+
+	private Amalgam extensionTarget;
+	
+	/*
+	 * Initializes a construction for a Lactiferous Antennae
+	 */
+	public LactiferousAntennaeConstruction(){
+		
+	}
+	
+}
